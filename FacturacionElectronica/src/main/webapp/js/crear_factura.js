8f81b492c72ddd742a0ae8ffd9bf337a84faad37
FabianRosero18 <125752036+FabianRosero18@users.noreply.github.com>
2024-07-25 12:07:27 -0500

    Delete FacturacionElectronica directory

--- FacturacionElectronica/src/main/webapp/js/crear_factura.js
+++
@@ -1,4 +0,0 @@
-
-function enviar(){
-	alert("FACTURA ENVIADA AL CLIENTE Y A LA DIAN");
-}
--- FacturacionElectronica/target/FacturacionElectronica-1.0-SNAPSHOT/js/crear_cliente.js
+++
@@ -1,9 +0,0 @@
-function crearCliente(){
-
-var mensaje = document.getElementById('mensajeCreacion');
-
-mensaje.innerHTML = "<div class='mensaje'>aqui aparecera si el cliente fue creado, o si hay algun error con su respectivo codigo</div>";
-
-console.log("SI ESTA EJECUTANDO LA FUNCION");
-
-}
